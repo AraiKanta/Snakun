88e6b584e5b8969828cec28435e3f18b819bfa02
unknown <attckonk@gmail.com>
2019-12-07 20:03:20 +0900

    add sceneloader

---
+++ Snakun Unity Project/Assets/Script/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //追加した
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        SceneManager.LoadScene("GAME");
+    }
+}

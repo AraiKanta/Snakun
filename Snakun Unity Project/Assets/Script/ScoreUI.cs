1f68d8ead5b9de59d60458386c364e44e555fc51
unknown <attckonk@gmail.com>
2020-01-25 12:54:28 +0900

    score kasika

---
+++ Snakun Unity Project/Assets/Script/ScoreUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Text ScoreText;
+    //　フェードアウトするスピード
+    private float fadeOutSpeed = 3f;
+    //　移動値
+    [SerializeField]
+    private float moveSpeed = 0.4f;
+
+    void Start()
+    {
+        ScoreText = GetComponentInChildren<Text>();
+    }
+    void LateUpdate()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        ScoreText.color = Color.Lerp(ScoreText.color, new Color(1f, 0f, 0f, 0f), fadeOutSpeed * Time.deltaTime);
+
+        if (ScoreText.color.a <= 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Snakun Unity Project/Assets/Script/TakeScore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TakeScore : MonoBehaviour
+{
+
+    //　ScoreUIプレハブ
+    [SerializeField]
+    private GameObject ScoreUI;
+
+    private Transform parentTrans = null;
+
+    private void Start()
+    {
+        parentTrans = GameObject.Find("ScoreRoot").transform;
+    }
+
+    public void Score(Collider2D collision)
+    {
+        //　ScoreUIをインスタンス化。登場位置は接触したコライダの中心からカメラの方向に少し寄せた位置
+        var obj = Instantiate<GameObject>(ScoreUI, Vector3.zero, Quaternion.identity);
+        obj.transform.SetParent(parentTrans);
+        obj.transform.localPosition = Vector3.zero;
+    }
+}

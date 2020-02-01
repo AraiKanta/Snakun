eddba7342dd0653e8792fde18670c4ceea744cf1
unknown <attckonk@gmail.com>
2020-02-01 09:38:51 +0900

    add subtotal

--- Snakun Unity Project/Assets/Script/GetScore.cs
+++ Snakun Unity Project/Assets/Script/GetScore.cs
@@ -9,7 +9,12 @@ public class GetScore : MonoBehaviour
     double s;
     [SerializeField] Text m_comboText;
     int c;
-
+    [SerializeField] Text redFlower;
+    int rf;
+    [SerializeField] Text blueFlower;
+    int bf;
+    [SerializeField] Text yellowFlower;
+    int yf;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +22,18 @@ public class GetScore : MonoBehaviour
         Debug.Log(s);
         c = GameManager1.GetC();
         Debug.Log(c);
+        rf = GameManager1.GetRF();
+        Debug.Log(rf);
+        bf = GameManager1.GetBF();
+        Debug.Log(bf);
+        yf = GameManager1.GetYF();
+        Debug.Log(yf);
 
         m_scoreText.text = "スコア:" + s.ToString("");
         m_comboText.text = "コンボ:" + c.ToString("");
+        redFlower.text = "赤い花:" + rf.ToString("");
+        blueFlower.text = "青い花:" + bf.ToString("");
+        yellowFlower.text = "黄色い花:" + yf.ToString("");
     }
 
     // Update is called once per frame
